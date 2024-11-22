8ab5e7fef734eccbed4cb315e54f3cc15ecfce58
Creastv <creastv@gail.com>
2024-11-22 10:55:21 +0100

    dev serwer

--- blocks/slider/slider.js
+++ blocks/slider/slider.js
@@ -3,10 +3,11 @@ var swiper = new Swiper(".slider-carousel", {
     slidesPerView: 1,
     // spaceBetween: 30,
     loop: true,
-    // autoplay: {
-    //   delay: 4500,
-    //   disableOnInteraction: false
-    // },
+    effect: "fade",
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true
