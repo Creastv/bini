a0f14f98487bb5257971ed2776f81f4091f79547
Creastv <creastv@gail.com>
2024-11-30 08:33:18 +0100

    dev serwer

--- func/cpt.php
+++ func/cpt.php
@@ -4,8 +4,8 @@
 function create_collection_taxonomy()
 {
     $labels = array(
-        'name'              => _x('Collections', 'taxonomy general name'),
-        'singular_name'     => _x('Collection', 'taxonomy singular name'),
+        'name'              => _x('Collections', 'go'),
+        'singular_name'     => _x('Collection', 'go'),
         'search_items'      => __('Search Collections'),
         'all_items'         => __('All Collections'),
         'parent_item'       => __('Parent Collection'),
@@ -31,20 +31,22 @@ function create_collection_taxonomy()
 }
 add_action('init', 'create_collection_taxonomy');
 
+
+
 // Rejestracja niestandardowej taksonomii Collection i przypisanie jej do produktów WooCommerce
 function create_tkaniny_taxonomy()
 {
     $labels = array(
-        'name'              => _x('Tkaniny', 'taxonomy general name'),
-        'singular_name'     => _x('Collection', 'taxonomy singular name'),
+        'name'              => _x('Tkaniny', 'go'),
+        'singular_name'     => _x('Tkaniny', 'go'),
         'search_items'      => __('Search Tkaniny'),
         'all_items'         => __('All Tkaniny'),
-        'parent_item'       => __('Parent Collection'),
-        'parent_item_colon' => __('Parent Collection:'),
-        'edit_item'         => __('Edit Collection'),
-        'update_item'       => __('Update Collection'),
-        'add_new_item'      => __('Add New Collection'),
-        'new_item_name'     => __('New Collection Name'),
+        'parent_item'       => __('Parent Tkaniny'),
+        'parent_item_colon' => __('Parent Tkaniny:'),
+        'edit_item'         => __('Edit Tkaniny'),
+        'update_item'       => __('Update Tkaniny'),
+        'add_new_item'      => __('Add New Tkaniny'),
+        'new_item_name'     => __('New Tkaniny Name'),
         'menu_name'         => __('Tkaniny'),
     );
 
@@ -62,6 +64,28 @@ function create_tkaniny_taxonomy()
 }
 add_action('init', 'create_tkaniny_taxonomy');
 
+
+// Cpt Opinie
+function create_opinie_cpt()
+{
+    $labels = array(
+        'name'          => __('Opinie', 'go'),
+        'singular_name' => __('Opinia', 'go'),
+        'menu_name'     => __('Opinie', 'go'),
+    );
+
+    $args = array(
+        'labels'        => $labels,
+        'public'        => true,
+        'has_archive'   => true,
+        'rewrite'       => array('slug' => 'opinie'),
+        'supports'      => array('title', 'editor'), // Wystarczy zdjęcie wyróżniające
+        'menu_icon'     => 'dashicons-lightbulb',
+    );
+
+    register_post_type('opinie', $args);
+}
+add_action('init', 'create_opinie_cpt');
 
 // Cpt Inspirations
 function create_inspirations_cpt()
