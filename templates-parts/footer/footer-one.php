2288a87b0bad93586ff17988b6d1d03ac5e562ca
Creastv <creastv@gail.com>
2024-12-02 18:31:33 +0100

    dev serwer

--- templates-parts/footer/footer-one.php
+++ templates-parts/footer/footer-one.php
@@ -33,7 +33,8 @@
                         <div class="b-info__wrap">
                             <h4 class="h5"><?php _e('Metody płatności', 'go'); ?></h4>
                             <div class="b-info__wrap__logos">
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/imgpayments.png" alt="Payments">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/payments.png"
+                                    alt="Payments">
 
                             </div>
                             <h4 class="h5"><?php _e('Kurierzy', 'go'); ?></h4>
