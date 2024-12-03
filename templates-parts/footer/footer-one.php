8c8df2fe881e25967d15bc73ac6cd514a06a4fcb
Creastv <creastv@gail.com>
2024-12-03 12:50:53 +0100

    dev serwer

--- templates-parts/footer/footer-one.php
+++ templates-parts/footer/footer-one.php
@@ -17,33 +17,34 @@
             </div>
             <div class="col">
                 <?php if (is_active_sidebar('footer-2')) : ?>
-                    <?php dynamic_sidebar('footer-2'); ?>
+                <?php dynamic_sidebar('footer-2'); ?>
                 <?php else : ?>
                 <?php endif; ?>
                 <?php if (is_active_sidebar('footer-3')) : ?>
-                    <?php dynamic_sidebar('footer-3'); ?>
+                <?php dynamic_sidebar('footer-3'); ?>
                 <?php else : ?>
                 <?php endif; ?>
             </div>
             <div class="col">
                 <?php if (is_active_sidebar('footer-4')) : ?>
-                    <?php dynamic_sidebar('footer-4'); ?>
+                <?php dynamic_sidebar('footer-4'); ?>
                 <?php else : ?>
-                    <div class="b-info">
-                        <div class="b-info__wrap">
-                            <h4 class="h5"><?php _e('Metody płatności', 'go'); ?></h4>
-                            <div class="b-info__wrap__logos">
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/payments.png"
-                                    alt="Payments">
+                <div class="b-info">
+                    <div class="b-info__wrap">
 
-                            </div>
+                        <div class="b-info__wrap__logos">
+                            <h4 class="h5"><?php _e('Metody płatności', 'go'); ?></h4>
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/payments.png"
+                                alt="Payments">
+                        </div>
+
+                        <div class="b-info__wrap__logos">
                             <h4 class="h5"><?php _e('Kurierzy', 'go'); ?></h4>
-                            <div class="b-info__wrap__logos">
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/delivery.png"
-                                    alt="Delivery">
-                            </div>
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/delivery.png"
+                                alt="Delivery">
                         </div>
                     </div>
+                </div>
                 <?php endif; ?>
             </div>
         </div>
