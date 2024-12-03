4bea7289f76bc25574eef612c8ed644c8bb7738a
Creastv <creastv@gail.com>
2024-12-03 12:13:12 +0100

    dev serwer

--- blocks/slider/slider.php
+++ blocks/slider/slider.php
@@ -16,24 +16,26 @@ if (!empty($block['anchor'])) {
         <div class="swiper slider-carousel">
             <div class="swiper-wrapper">
                 <?php foreach ($slider as $slide) { ?>
-                    <div class="swiper-slide">
-                        <div class="item">
-                            <div class="img" style="background-image: url(<?php echo $slide['zdjecie']; ?>);">
-                                <div class="content">
-                                    <?php if ($slide['title']) { ?>
-                                        <h1 class="big-title"><?php echo $slide['title']; ?></h1>
-                                    <?php } ?>
-                                    <?php if ($slide['desc']) { ?>
-                                        <p> <?php echo $slide['desc']; ?></p>
-                                    <?php } ?>
-                                    <?php if ($slide['link']) { ?>
-                                        <a class="btn-main" href="<?php echo esc_url($slide['link']['url']); ?>"
-                                            target="<?php echo esc_attr($slide['link']['target'] ? $slide['link']['target'] : '_self'); ?>"><?php echo esc_html($slide['link']['title']); ?></a>
-                                    <?php } ?>
-                                </div>
+                <div class="swiper-slide">
+                    <div class="item">
+                        <div class="img" style="background-image: url(<?php echo $slide['zdjecie']; ?>);">
+                            <div class="content">
+                                <?php if ($slide['title']) { ?>
+                                <<?php echo $slide['tag']; ?> class="h1"><?php echo $slide['title']; ?>
+                                </<?php echo
+                                                                                                                $slide['tag']; ?>>
+                                <?php } ?>
+                                <?php if ($slide['desc']) { ?>
+                                <p> <?php echo $slide['desc']; ?></p>
+                                <?php } ?>
+                                <?php if ($slide['link']) { ?>
+                                <a class="btn-main" href="<?php echo esc_url($slide['link']['url']); ?>"
+                                    target="<?php echo esc_attr($slide['link']['target'] ? $slide['link']['target'] : '_self'); ?>"><?php echo esc_html($slide['link']['title']); ?></a>
+                                <?php } ?>
                             </div>
                         </div>
                     </div>
+                </div>
                 <?php } ?>
             </div>
         </div>
